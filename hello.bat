537e2ee2a6238daa60a43f3398dea7135889c8d1
AbdulRaufAl-hassan101010 <abdulraufalhassan101010@gmail.com>
2023-11-12 10:33:05 +0000

    created first batch files to display hello world and pass arg

---
+++ hello.bat
@@ -0,0 +1,2 @@
+@python C:\Users\Abdul-Rauf\Documents\python\automation\hello.py %*
+@pause
---
+++ hello.py
@@ -0,0 +1,12 @@
+# windows
+#! python3
+
+# os x
+#! /usr/bin/env python3
+
+# linux
+#! /usr/bin/pythons
+
+from sys import argv
+print("Hello world!! ")
+print(argv)
